e47329b456e18dc35ef08398b2674ecb6e0c1636
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-23 22:09:52 +0100

    created the test class and java main class file. - Will be testing the empty list for classes going forward - For now playing around to have a refresher

---
+++ src/main/java/ToDoApp.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoApp {
+
+    List<String> taskList;
+
+    public ToDoApp() {
+        this.taskList = new ArrayList<>();
+    }
+}
---
+++ src/test/java/ToDoAppTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ToDoAppTest {
+
+    ToDoApp taskClass = new ToDoApp();
+    List<String> taskList = new ArrayList<>();
+    @Test
+    void addTaskToList() {
+
+        assertTrue(taskList, taskClass.);
+    }
+}
